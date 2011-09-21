013a5109b636c4b12de3f9babdddec592c00a01b
Michael de Silva <michael@mwdesilva.com>
2011-09-21 03:16:33 +0300

    untracked development sqlite db and tweaked exception handler

--- config/initializers/exception_handler.rb
+++ config/initializers/exception_handler.rb
@@ -1,5 +1,5 @@
 ActiveSupport::Notifications.subscribe "process_action.action_controller" do |name, start, finish, id, payload|
-  Rails.logger.debug "EXCEPTION: #{payload[:exception]}"
+  Rails.logger.debug "EXCEPTION: #{payload[:exception]}" if Rails.env.development? && !payload[:exception].blank?
   if payload[:exception]
     name, message = *payload[:exception]
     EpicFail::Failure.create!(:message => message)
